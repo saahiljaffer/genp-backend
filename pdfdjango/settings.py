309b6a36272b7142769f611fa516a940cdaab128
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-08 21:41:54 -0400

    CORS

--- pdfdjango/settings.py
+++ pdfdjango/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pdfdjango.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
+    "https://pdf.saahiljaffer.com",
     "https://sub.example.com",
     "http://localhost:8080",
     "http://localhost:3000",
@@ -131,7 +131,6 @@ django_on_heroku.settings(locals())
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
