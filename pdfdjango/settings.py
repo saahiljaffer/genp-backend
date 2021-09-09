a7fa367363bcb899c7c0a2ab4e53a334956685f6
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-08 21:36:06 -0400

    rename

--- pdfdjango/settings.py
+++ pdfdjango/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
