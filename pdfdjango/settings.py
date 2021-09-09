cb9a0f01c41ac2f6dd31f2ff63231972f678ecf2
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-08 21:31:55 -0400

    Add Procfile

--- pdfdjango/settings.py
+++ pdfdjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
