0adf6fd231ba147a1a6d914f189876a0eedc2517
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-11 16:58:06 -0400

    Adds CSS support

--- pdf/views.py
+++ pdf/views.py
@@ -2,15 +2,17 @@ import functools
 
 from django.conf import settings
 from django.http import HttpResponse, FileResponse
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 import tempfile
 import json
+
 
 def generate_pdf(request):
     data = json.loads(request.body)
     """Generate pdf."""
-    html = HTML(string=data['text'])
-    pdf_file = html.write_pdf()
-    response = HttpResponse(pdf_file, content_type='application/pdf')
-    response['Content-Disposition'] = 'filename="home_page.pdf"'
+    html = HTML(string=data["text"])
+    css = CSS(string=data["css"])
+    pdf_file = html.write_pdf(stylesheets=[css])
+    response = HttpResponse(pdf_file, content_type="application/pdf")
+    response["Content-Disposition"] = 'filename="home_page.pdf"'
     return response
